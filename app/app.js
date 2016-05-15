bedfdd5c57137f0c55026b80fff5093663195c29
Philippe Tjon - A - Hen <philippe@tjonahen.nl>
2016-05-15 14:36:52 +0200

    Added basic preset functionality

--- app/app.js
+++ app/app.js
@@ -19,68 +19,94 @@
  */
 
 (function () {
-   angular.module('ivonet-presentations-app', ['ivoMarkdown']).config(function (ivoMarkdownConfigProvider) {
-      ivoMarkdownConfigProvider.config({
-         tables: true,
-         parseImgDimensions: true,
-         simplifiedAutoLink: true,
-         tasklists: true,
-         smoothLivePreview: true,
-         strikethrough: true
-      });
-      ivoMarkdownConfigProvider.hljsOptions({classPrefix: 'ivonet-', tabreplace: '    '});
+    angular.module('ivonet-presentations-app', ['ivoMarkdown']).config(function (ivoMarkdownConfigProvider) {
+        ivoMarkdownConfigProvider.config({
+            tables: true,
+            parseImgDimensions: true,
+            simplifiedAutoLink: true,
+            tasklists: true,
+            smoothLivePreview: true,
+            strikethrough: true
+        });
+        ivoMarkdownConfigProvider.hljsOptions({classPrefix: 'ivonet-', tabreplace: '    '});
 
-   }).controller('IvoNetPresentationsController', IvoNetPresentationsController);
+    }).controller('IvoNetPresentationsController', IvoNetPresentationsController);
 
-   IvoNetPresentationsController.$inject = [
-      '$window',
-      '$http'
-   ];
+    IvoNetPresentationsController.$inject = [
+        '$window',
+        '$http'
+    ];
 
-   function IvoNetPresentationsController($window, $http) {
-      var that = this;
-      that.message = '';
-      that.baseurl =
-           $window.location.protocol + '//' + $window.location.hostname + ':' + $window.location.port;
-      that.presenterNotes = false;
-      that.center = false;
-      that.mouseWheel = false;
-      that.controls = true;
-      that.slideNumber = false;
-      that.print = false;
-      that.slide = undefined;
+    function IvoNetPresentationsController($window, $http) {
+        var that = this;
+        that.message = '';
+        that.baseurl =
+                $window.location.protocol + '//' + $window.location.hostname + ':' + $window.location.port;
+        that.slide = undefined;
 
-      that.model = {};
-      $http.get('/model.json').success(function (data) {
-         that.model = data;
-      });
+        function setDefaults() {
+            that.presenterNotes = false;
+            that.center = false;
+            that.mouseWheel = false;
+            that.controls = true;
+            that.slideNumber = false;
+            that.print = false;
+        };
 
-      that.go = function () {
-         if (that.slide == undefined) {
-            that.message="You must select a slide.";
-            return;
-         }
-         var url = that.baseurl;
-         url += that.model.template;
-         url += "?showNotes=" + that.presenterNotes;
-         url += "&theme=" + that.model.theme;
-         url += "&center=" + that.center;
-         if (that.mouseWheel) {
-             url += "&mouseWheel=true"; // false is default
-         }
-         if (that.slideNumber) {
-             url += "&slideNumber=true"; // false is default
-         }
-         if (!that.controls) {
-             url += "&controls=false"; // true is default
-         }
-         url += "&transition=" + that.model.transition;
-         url += '&title=' + that.slide.title;
-         if (that.print) {
-            url += "&print-pdf=true";
-         }
-         url += "&slideshow=" + that.slide.file;
-         $window.open(url, '_blank');
-      };
-   }
+        setDefaults();
+
+        that.model = {};
+        $http.get('/model.json').success(function (data) {
+            that.model = data;
+        });
+
+        that.go = function () {
+            if (that.slide === undefined) {
+                that.message = "You must select a slide.";
+                return;
+            }
+            var url = that.baseurl;
+            url += that.model.template;
+            url += "?showNotes=" + that.presenterNotes;
+            url += "&theme=" + that.model.theme;
+            url += "&center=" + that.center;
+            if (that.mouseWheel) {
+                url += "&mouseWheel=true"; // false is default
+            }
+            if (that.slideNumber) {
+                url += "&slideNumber=true"; // false is default
+            }
+            if (!that.controls) {
+                url += "&controls=false"; // true is default
+            }
+            url += "&transition=" + that.model.transition;
+            url += '&title=' + that.slide.title;
+            if (that.print) {
+                url += "&print-pdf=true";
+            }
+            url += "&slideshow=" + that.slide.file;
+            $window.open(url, '_blank');
+        };
+
+        that.onSelect = function (slide) {
+            // retrieve preset if available
+            if (slide.preset !== undefined) {
+                $http.get(slide.preset).success(function (data) {
+
+                    if (data.controls !== undefined) {
+                        that.controls = data.controls;
+                    }
+                    if (data.mouseWheel !== undefined) {
+                        that.mouseWheel = data.mouseWheel;
+                    }
+                    if (data.slideNumber !== undefined) {
+                        that.slideNumber = data.slideNumber;
+                    }
+
+                });
+            } else {
+                setDefaults();
+            }
+        };
+    }
 })();
