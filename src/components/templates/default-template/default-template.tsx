42aade4e2ba621e9296ccbfc94509929f38706d6
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-12-16 19:08:34 +0100

    chore: clean up

--- src/components/templates/default-template/default-template.tsx
+++ src/components/templates/default-template/default-template.tsx
@@ -1,74 +1,75 @@
-import { component$, useVisibleTask$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
-import Reveal from "reveal.js";
-import RevealMarkdown from "reveal.js/plugin/markdown/markdown";
+import { component$ } from "@builder.io/qwik";
+// import { useLocation } from "@builder.io/qwik-city";
+// import Reveal from "reveal.js";
+// import RevealMarkdown from "reveal.js/plugin/markdown/markdown";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import type { CustomizedOptions } from "~/types/reveal.js";
+// import type { CustomizedOptions } from "~/types/reveal.js";
 
-const removePublicPrefix = (path: string) =>
-  path.indexOf("/public/") === 0 ? path.substring("/public/".length - 1) : path;
+// const removePublicPrefix = (path: string) =>
+//   path.indexOf("/public/") === 0 ? path.substring("/public/".length - 1) : path;
 
-const DEFAULT_SLIDES_PATH =
-  "/slides/1.%20How%20to%20%20Scooch/1.%20How%20to%20Scooch.md";
+// const DEFAULT_SLIDES_PATH =
+//   "/slides/1.%20How%20to%20%20Scooch/1.%20How%20to%20Scooch.md";
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const DefaultTemplate = component$(() => {
-  const { url } = useLocation();
-  const slideshowParam = url.searchParams.get("slideshow");
-  const slideshow = slideshowParam
-    ? removePublicPrefix(slideshowParam)
-    : DEFAULT_SLIDES_PATH;
+  return <div></div>;
+  // const { url } = useLocation();
+  // const slideshowParam = url.searchParams.get("slideshow");
+  // const slideshow = slideshowParam
+  //   ? removePublicPrefix(slideshowParam)
+  //   : DEFAULT_SLIDES_PATH;
 
-  // eslint-disable-next-line qwik/no-use-visible-task
-  useVisibleTask$(async () => {
-    await import("reveal.js-plugins/customcontrols/plugin");
-    await import("reveal.js-plugins/chalkboard/plugin");
-    await import("reveal.js-plugins/customcontrols/style.css");
-    await import("reveal.js-plugins/chalkboard/style.css");
+  // // eslint-disable-next-line qwik/no-use-visible-task
+  // useVisibleTask$(async () => {
+  //   await import("reveal.js-plugins/customcontrols/plugin");
+  //   await import("reveal.js-plugins/chalkboard/plugin");
+  //   await import("reveal.js-plugins/customcontrols/style.css");
+  //   await import("reveal.js-plugins/chalkboard/style.css");
 
-    const options: CustomizedOptions = {
-      customcontrols: {
-        controls: [
-          {
-            icon: '<i class="fa fa-pen-square"></i>',
-            title: "Toggle chalkboard (B)",
-            action: "RevealChalkboard.toggleChalkboard();",
-          },
-          {
-            icon: '<i class="fa fa-pen"></i>',
-            title: "Toggle notes canvas (C)",
-            action: "RevealChalkboard.toggleNotesCanvas();",
-          },
-        ],
-      },
-      plugins: [
-        RevealMarkdown,
-        window.RevealChalkboard,
-        window.RevealCustomControls,
-      ],
-    };
+  //   const options: CustomizedOptions = {
+  //     customcontrols: {
+  //       controls: [
+  //         {
+  //           icon: '<i class="fa fa-pen-square"></i>',
+  //           title: "Toggle chalkboard (B)",
+  //           action: "RevealChalkboard.toggleChalkboard();",
+  //         },
+  //         {
+  //           icon: '<i class="fa fa-pen"></i>',
+  //           title: "Toggle notes canvas (C)",
+  //           action: "RevealChalkboard.toggleNotesCanvas();",
+  //         },
+  //       ],
+  //     },
+  //     plugins: [
+  //       RevealMarkdown,
+  //       window.RevealChalkboard,
+  //       window.RevealCustomControls,
+  //     ],
+  //   };
 
-    Reveal.initialize(options);
-  });
+  //   Reveal.initialize(options);
+  // });
 
-  return (
-    <div class="reveal">
-      <div class="slides">
-        <section
-          data-markdown={slideshow}
-          data-separator="^---$"
-          data-separator-vertical="^--$"
-          data-separator-notes="^Note:"
-        ></section>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div class="reveal">
+  //     <div class="slides">
+  //       <section
+  //         data-markdown={slideshow}
+  //         data-separator="^---$"
+  //         data-separator-vertical="^--$"
+  //         data-separator-notes="^Note:"
+  //       ></section>
+  //     </div>
+  //   </div>
+  // );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Scooch",
   // Font awesome is required for the chalkboard plugin
   scripts: [
     {
