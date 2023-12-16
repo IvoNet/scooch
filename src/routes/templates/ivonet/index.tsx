8e3e5c204217455d54f7930485fcd571b5586f25
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-12-16 19:43:10 +0100

    chore: add ivonet styles

--- src/routes/templates/ivonet/index.tsx
+++ src/routes/templates/ivonet/index.tsx
@@ -1,15 +1,22 @@
 import { component$ } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
-import "reveal.js/dist/reveal.css";
-import "reveal.js/dist/theme/black.css";
 import { RevealSlidesHeadOptions } from "~/components/reveal-slides/head";
 import { RevealSlides } from "~/components/reveal-slides/reveal-slides";
+import "reveal.js/dist/reveal.css";
+import "./ivonet.css";
+
+import ImgWolfLogoLeftWhiteSm from "./wolf-logo-left-white-sm.svg?jsx";
 
 export default component$(() => {
   return (
-    <div class="reveal">
-      <RevealSlides />
-    </div>
+    <>
+      <div style="position: fixed; top: 0; width:100%;">
+        <ImgWolfLogoLeftWhiteSm style="float: right" />
+      </div>
+      <div class="reveal">
+        <RevealSlides />
+      </div>
+    </>
   );
 });
 
