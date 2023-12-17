b4061530dc258956b7fdb2edfed9225cbcce1b5b
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-12-17 10:51:00 +0100

    chore: add themes

--- src/components/reveal-slides/load-theme.tsx
+++ src/components/reveal-slides/load-theme.tsx
@@ -1,6 +1,6 @@
 export const THEMES = [
   "black",
-  "fixed",
+  // "fixed",
   "ivonet",
   "beige",
   "blood",
@@ -20,7 +20,40 @@ export const loadTheme = async (theme: ThemeOptions) => {
   if (theme === "black") {
     await import("reveal.js/dist/theme/black.css");
   }
+  // if (theme === "fixed") {
+  //   await import("reveal.js/dist/theme/fixed.css");
+  // }
+  if (theme === "ivonet") {
+    await import("../../routes/templates/ivonet/ivonet.css?inline");
+  }
+  if (theme === "beige") {
+    await import("reveal.js/dist/theme/beige.css");
+  }
+  if (theme === "blood") {
+    await import("reveal.js/dist/theme/blood.css");
+  }
+  if (theme === "league") {
+    await import("reveal.js/dist/theme/league.css");
+  }
   if (theme === "moon") {
     await import("reveal.js/dist/theme/moon.css");
+  }
+  if (theme === "night") {
+    await import("reveal.js/dist/theme/night.css");
+  }
+  if (theme === "serif") {
+    await import("reveal.js/dist/theme/serif.css");
+  }
+  if (theme === "simple") {
+    await import("reveal.js/dist/theme/simple.css");
+  }
+  if (theme === "sky") {
+    await import("reveal.js/dist/theme/sky.css");
+  }
+  if (theme === "solarized") {
+    await import("reveal.js/dist/theme/solarized.css");
+  }
+  if (theme === "white") {
+    await import("reveal.js/dist/theme/white.css");
   }
 };
