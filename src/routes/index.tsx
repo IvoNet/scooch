39ddd9917578aa520f320468ba04d0762bcc4ac8
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-12-22 11:55:02 +0100

    feat: save preset

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import {
   routeLoader$,
   type DocumentHead,
   useLocation,
-  server$,
 } from "@builder.io/qwik-city";
 import Header from "~/components/starter/header/header";
 import styles from "./styles.css?inline";
@@ -16,13 +15,8 @@ import { fileToSlideConfig } from "~/util/file-to-slide-config";
 import { Checkbox } from "~/components/checkbox/checkbox";
 import { getQueryParamsFromState } from "~/util/get-query-params-from-state";
 import { PresetSelector } from "~/components/preset-selector/preset-selector";
-import { savePresetToFs } from "~/util/save-preset";
-
-const config = {
-  themesDir: "./templates",
-  templatesDir: "./templates",
-  slidesDir: "./public/slides",
-};
+import { config } from "~/util/constants";
+import { SavePresetButton } from "~/components/save-preset-button/save-preset-button";
 
 /**
  *
@@ -32,12 +26,6 @@ export const useSlides = routeLoader$(async () => {
   const files = await walkSyncPromise(config.slidesDir);
   return files.map(fileToSlideConfig).filter(isDefined);
 });
-
-const savePreset = server$(
-  async (title: string, template: string, params: URLSearchParams) => {
-    return await savePresetToFs(config.slidesDir, title, template, params);
-  }
-);
 
 export default component$(() => {
   useStyles$(styles);
@@ -205,34 +193,12 @@ export default component$(() => {
               >
                 Scooch it!
               </button>
-              <button
-                class="btn btn-secondary"
-                disabled={!selectedSlideshowSignal.value}
-                onClick$={async () => {
-                  const response = await savePreset(
-                    slidesSignal.value.find(
-                      (slide) => slide.file === selectedSlideshowSignal.value
-                    )?.title ?? "",
-                    selectedTemplateSignal.value,
-                    q
-                  );
-                  if (response.result === "ok") {
-                    alert("Preset saved");
-                  }
-                }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-floppy2"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v3.5A1.5 1.5 0 0 1 11.5 6h-7A1.5 1.5 0 0 1 3 4.5V1H1.5a.5.5 0 0 0-.5.5m9.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5z" />
-                </svg>{" "}
-                Save preset
-              </button>
+              <SavePresetButton
+                slides={slidesSignal.value}
+                selectedSlideshow={selectedSlideshowSignal.value}
+                selectedTemplate={selectedTemplateSignal.value}
+                params={q}
+              />
             </div>
           </div>
         </div>
